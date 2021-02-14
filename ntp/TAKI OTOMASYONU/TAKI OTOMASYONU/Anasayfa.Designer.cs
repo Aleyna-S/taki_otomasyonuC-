1fb1a86d86c4c4cc560d42e1d195e2bc284b8ce9
Aleyna-S <65609414+Aleyna-S@users.noreply.github.com>
2021-02-14 19:27:55 +0300

    Add files via upload

---
+++ ntp/TAKI OTOMASYONU/TAKI OTOMASYONU/Anasayfa.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace TAKI_OTOMASYONU
+{
+    partial class Anasayfa
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Anasayfa));
+            this.lbn_takiotomasyonu = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbn_takiotomasyonu
+            // 
+            this.lbn_takiotomasyonu.AutoSize = true;
+            this.lbn_takiotomasyonu.BackColor = System.Drawing.Color.Transparent;
+            this.lbn_takiotomasyonu.Font = new System.Drawing.Font("Showcard Gothic", 16.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbn_takiotomasyonu.ForeColor = System.Drawing.Color.Black;
+            this.lbn_takiotomasyonu.Location = new System.Drawing.Point(206, 9);
+            this.lbn_takiotomasyonu.Name = "lbn_takiotomasyonu";
+            this.lbn_takiotomasyonu.Size = new System.Drawing.Size(164, 35);
+            this.lbn_takiotomasyonu.TabIndex = 0;
+            this.lbn_takiotomasyonu.Text = "ALŞA TAKI";
+            this.lbn_takiotomasyonu.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Control;
+            this.button1.Font = new System.Drawing.Font("Georgia", 7.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(224, 88);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(126, 39);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Yönetici Giriş";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Georgia", 7.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(224, 164);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(126, 39);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Üye Giriş";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Georgia", 7.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(224, 241);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(126, 39);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Üye Kayıt Ol";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Anasayfa
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(567, 362);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lbn_takiotomasyonu);
+            this.Name = "Anasayfa";
+            this.Text = "Ana Sayfa";
+            this.Load += new System.EventHandler(this.Anasayfa_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbn_takiotomasyonu;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ ntp/TAKI OTOMASYONU/TAKI OTOMASYONU/Anasayfa.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TAKI_OTOMASYONU
+{
+    public partial class Anasayfa : Form
+    {
+        public Anasayfa()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lbn_sifre_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            YoneticiGiris yonet = new YoneticiGiris();
+            this.Hide();
+            yonet.ShowDialog();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            uyeol uye = new uyeol(); this.Hide(); uye.ShowDialog();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ÜyeGiris üye = new ÜyeGiris();
+            this.Hide();
+            üye.ShowDialog();
+        }
+
+        private void Anasayfa_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
